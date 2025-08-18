940f44333472262d90c1ef006a1d6992586c5943
Le-Meur-Mai <mai.lm@outlook.fr>
2025-08-18 10:36:55 +0200

    Modification of error messages

--- shell.c
+++ shell.c
@@ -14,10 +14,10 @@ int main(int ac, char **av, char **env)
 {
 	ssize_t check = 0;
 	size_t size_buffer = 0;
-	char *line = NULL;
-	char **_argv = NULL;
+	char *line = NULL, **_argv = NULL;
 	pid_t my_pid;
 	int status;
+	struct stat st;
 	(void)ac;
 
 	while (1)
@@ -33,18 +33,19 @@ int main(int ac, char **av, char **env)
 		}
 			line[(strlen(line) - 1)] = '\0';
 			_argv = argv_for_shell(line);
-			my_pid = fork();
-			if (my_pid == -1)
-				printf("Can't strat the program\n");
-			else if (my_pid == 0)
+			if (stat(_argv[0], &st) == -1 && isatty(STDIN_FILENO) == 1)
+				printf("%s: No such file or directory\n", av[0]);
+			else if ((my_pid = fork()) == 0)
 			{
 				if (execve(line, _argv, env) == -1)
 				{
-					printf("%s: 1: %s: not found\n", av[0], _argv[0]);
-					return (-1);
+						printf("%s: 1: %s: not found\n", av[0], _argv[0]);
+						return (-1);
 				}
 				return (0);
 			}
+			if (my_pid == -1)
+				printf("Can't start the program\n");
 			wait(&status);
 	}
 	free(_argv);
